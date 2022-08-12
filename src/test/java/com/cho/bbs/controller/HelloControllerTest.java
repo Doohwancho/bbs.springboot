c7c153070014b8c43a6dd2c096fe274b8f11287e
Doohwancho <doohwancho526@gmail.com>
2022-08-12 21:44:59 +0900

    test: hello controller test에서 junit4 -> junit5

--- src/test/java/com/cho/bbs/controller/HelloControllerTest.java
+++ src/test/java/com/cho/bbs/controller/HelloControllerTest.java
@@ -1,18 +1,18 @@
 package com.cho.bbs.controller;
 
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
     @Autowired
@@ -22,8 +22,8 @@ public class HelloControllerTest {
     public void hello를_리턴한다() throws  Exception {
         String hello = "Hello";
 
-        mvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(hello));
+        mvc.perform(MockMvcRequestBuilders.get("/hello"))
+                        .andExpect(MockMvcResultMatchers.status().isOk())
+                        .andExpect(MockMvcResultMatchers.content().string(hello));
     }
 }
