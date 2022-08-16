7f1c00e0b20fbdc8be84a39e772ee9a30aa559d1
Doohwancho <doohwancho526@gmail.com>
2022-08-16 19:58:20 +0900

    refactor: test code 이름 명확하게 변경

--- src/test/java/com/cho/bbs/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/cho/bbs/domain/posts/PostsRepositoryTest.java
@@ -17,15 +17,20 @@ public class PostsRepositoryTest {
     @Autowired
     PostsRepository postsRepository;
 
+    @AfterAll
+    public void 테스트_게시글_삭제(){
+        postsRepository.deleteAll();
+    }
+
     @Test
-    public void postsRepository_커넥션_테스트(){
+    public void postsRepository_의존성_주입_테스트(){
         System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$");
         System.out.println(postsRepository.getClass().getName());
         System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$");
     }
 
     @Test
-    public void 빌더_작동_여부_확인() {
+    public void 롬복_빌더_작동_여부_확인() {
         //given
         String title = "hello";
         String content = "hello world";
@@ -51,42 +56,38 @@ public class PostsRepositoryTest {
     }
 
 
+    @Test
+    public void 게시글_저장_후_불러오기(){
+        //given
+        String title = "hello";
+        String content = "hello world";
+        String author = "cho";
 
-    @AfterAll
-    public void 테스트_게시글_삭제(){
-        postsRepository.deleteAll();
+        Posts post = new Posts()
+                .builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+
+        postsRepository.save(post);
+
+        //when
+        List<Posts> postList = postsRepository.findAll();
+
+
+        //then
+        Posts result = postList.get(0);
+
+        assertThat(result.getTitle()).isNotNull();
+        assertThat(result.getTitle()).isEqualTo(title);
+
+        assertThat(result.getContent()).isNotNull();
+        assertThat(result.getContent()).isEqualTo(content);
+
+        assertThat(result.getAuthor()).isNotNull();
+        assertThat(result.getAuthor()).isEqualTo(author);
     }
 
-//    @Test
-//    public void 게시글_저장_후_불러오기(){
-//        //given
-//        String title = "hello";
-//        String content = "hello world";
-//        String author = "cho";
-//
-//        Posts post = new Posts()
-//                .builder()
-//                .title(title)
-//                .content(content)
-//                .author(author)
-//                .build();
-//
-//        postsRepository.save(post);
-//
-//        //when
-//        List<Posts> postList = postsRepository.findAll();
-//
-//
-//        //then
-//        Posts result = postList.get(0);
-//
-//        assertThat(result.getTitle()).isNotNull();
-//        assertThat(result.getTitle()).isEqualTo(title);
-//
-//        assertThat(result.getContent()).isNotNull();
-//        assertThat(result.getContent()).isEqualTo(content);
-//
-//        assertThat(result.getAuthor()).isNotNull();
-//        assertThat(result.getAuthor()).isEqualTo(author);
-//    }
+
 }
