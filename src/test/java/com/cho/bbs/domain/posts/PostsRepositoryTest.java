69537ec2667dbfd03876f610eede16929a0d7583
Doohwancho <doohwancho526@gmail.com>
2022-08-16 20:45:16 +0900

    test: pagination

--- src/test/java/com/cho/bbs/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/cho/bbs/domain/posts/PostsRepositoryTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -22,6 +26,7 @@ public class PostsRepositoryTest {
         postsRepository.deleteAll();
     }
 
+    @Order(1)
     @Test
     public void postsRepository_의존성_주입_테스트(){
         System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$");
@@ -29,6 +34,7 @@ public class PostsRepositoryTest {
         System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$");
     }
 
+    @Order(2)
     @Test
     public void 롬복_빌더_작동_여부_확인() {
         //given
@@ -56,6 +62,7 @@ public class PostsRepositoryTest {
     }
 
 
+    @Order(3)
     @Test
     public void 게시글_저장_후_불러오기(){
         //given
@@ -89,5 +96,39 @@ public class PostsRepositoryTest {
         assertThat(result.getAuthor()).isEqualTo(author);
     }
 
+    @Order(4)
+    @Test
+    public void paging_테스트(){
+        //given
+        for(int i = 0; i < 30; i++){
+            Posts post = new Posts()
+                    .builder()
+                    .title("title "+i)
+                    .content("content "+i)
+                    .author("author: "+i)
+                    .build();
+            postsRepository.save(post);
+        }
 
+        Pageable pageable = PageRequest.of(0, 10);
+
+
+        //when
+        Page<Posts> results = postsRepository.findAll(pageable);
+
+        //then
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&");
+        System.out.println(results);
+        System.out.println(results.getTotalPages()); //총 몇 페이지
+        System.out.println(results.getTotalElements()); //전체 개수
+        System.out.println(results.getNumber()); //현재 페이지 번호
+        System.out.println(results.getSize()); //페이지당 데이터 개수
+        System.out.println(results.hasNext()); //다음 페이지 존재 여부
+        System.out.println(results.isFirst()); //시작 페이지 여부
+
+        for(Posts post: results.getContent()){
+            System.out.println(post.getAuthor());
+        }
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&");
+    }
 }
